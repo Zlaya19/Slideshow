fb026421167dceba41de234611f4f2256c9da9af
Zlatan Gavranovic <zlatangavranovic1906@gmail.com>
2021-11-19 19:10:57 +0100

    images added

---
+++ imageSlider.js
@@ -0,0 +1,5 @@
+const display = document.getElementById('output');
+const btnTop = document.getElementById('btnLast');
+const btnBottom = document.getElementById('btnNext');
+
+const imagesArray = ['code 1','code 2','code 3','code 4','code 5','code 6','code 7'];
