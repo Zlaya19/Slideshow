5d528447b1d46d6aa8a70dd9c17d9cf9f7a6deda
Zlatan Gavranovic <zlatangavranovic1906@gmail.com>
2021-11-20 20:20:35 +0100

    logic and details added

--- imageSlider.js
+++ imageSlider.js
@@ -1,5 +1,45 @@
 const display = document.getElementById('output');
 const btnTop = document.getElementById('btnLast');
 const btnBottom = document.getElementById('btnNext');
+const body = document.querySelector('body');
 
-const imagesArray = ['code 1','code 2','code 3','code 4','code 5','code 6','code 7'];
+const image = ['code1','code2','code3','code4','code5','code6','code7'];
+
+let imageIndex = 0;
+
+window.addEventListener('load', loadImage(image[imageIndex]));
+
+
+function loadImage(image){
+    display.style.backgroundImage = `url('images/${image}.jpg')`;
+    display.style.transition = '2s';
+}
+
+
+btnTop.addEventListener('click', previmage);
+btnBottom.addEventListener('click', nextimage);
+
+
+// Previous image
+function previmage() {
+    imageIndex--;
+  
+    if (imageIndex < 0) {
+      imageIndex = image.length - 1;
+    }
+  
+    loadImage(image[imageIndex]);
+
+  }
+  
+  // Next image
+  function nextimage() {
+    imageIndex++;
+  
+    if (imageIndex > image.length - 1) {
+      imageIndex = 0;
+    }
+  
+    loadImage(image[imageIndex]);
+
+  }
